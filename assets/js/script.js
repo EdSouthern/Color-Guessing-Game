059450c2abaaaa6be5a595bf428c74cf44490fb1
EdSouthern <Edward.J.Southern@outlook.com>
2021-11-23 09:42:16 +0000

    Add code for checking if the user has selcted the right color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// When the dom has finished loading the color question is set 
+onLoad()
 /**
  * Generates a random interger between 0-255
  */
@@ -31,7 +33,61 @@ function colorToRGBString(color) {
  */
 function setColorQuestion () {
     const color = generateColor();
-    console.log(color);
     const rgbString = colorToRGBString(color);
     document.getElementById("random-color").innerHTML = rgbString;
+    return color;
+}
+
+/**
+ * A function to randomise an arrary found on stack overflow.
+ */
+function randomizeArray(array) {
+    let currentIndex = array.length,  randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {      
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+      
+    return array;
+}
+
+function onLoad() {
+    const questionColor = setColorQuestion();
+
+    let boxColors = [
+        questionColor,
+        generateColor(),
+        generateColor(),
+        generateColor(),
+        generateColor(),
+        generateColor(),
+    ]
+
+    boxColors = randomizeArray(boxColors);
+
+    console.log(boxColors);
+
+    let boxes = document.querySelectorAll(".box");
+
+    for( let i=0; i<boxes.length; i++) {
+        boxes[i].style.backgroundColor = colorToRGBString(boxColors[i]);
+        boxes[i].addEventListener('click', function() {
+            checkColor(boxColors[i])
+        });
+        
+        onLoad()
+    }
+
+    function checkColor(boxColor) {
+        if(boxColor.r === questionColor.r && boxColor.g === questionColor.g && boxColor.b === questionColor.b) {
+            alert('You won!!!!')
+        } else {
+            alert('Wrong answer');
+        }
+    }
 }
